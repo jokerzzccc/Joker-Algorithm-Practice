394a66826501e35d8c6d24403513eee47739caa6
joker <joker_monster@foxmail.com>
2023-09-12 21:20:55 +0800

    feat: 每日一题：leetcode1462;

---
+++ src/main/java/com/joker/algorithm/leetcode1240.java
@@ -0,0 +1,28 @@
+//package com.joker.algorithm;
+//
+///**
+// * <p>
+// * 铺瓷砖
+// * </p>
+// *
+// * @author admin
+// * @date 2023/7/10
+// */
+//public class leetcode1240 {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    /**
+//     * 解法一：
+//     */
+//    private static class Solution01 {
+//
+//        public int tilingRectangle(int n, int m) {
+//
+//        }
+//
+//    }
+//
+//}
